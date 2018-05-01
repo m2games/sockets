89a3bb1a246eed642ec27d2747d590596a033a2d
matiTechno <mateusz.macieja8@gmail.com>
2018-05-01 12:30:53 +0200

    fix, working now

--- main.cpp
+++ main.cpp
@@ -8,6 +8,13 @@
 
 struct Address
 {
+    Address() = default;
+    Address(unsigned char a, unsigned char b, unsigned char c, unsigned char d,
+            unsigned short port): port(port)
+    {
+        setIp(a, b, c, d);
+    }
+
     void setIp(unsigned char a, unsigned char b, unsigned char c, unsigned char d)
     {
         ip = (a << 24) | (b << 16) | (c << 8) | d;
@@ -19,80 +26,10 @@ struct Address
 
 struct Socket
 {
-    bool open(unsigned short port = 0)
-    {
-        const int handle = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-        
-        if(handle <= 0)
-        {
-            printf("failed to create socket\n");
-            return false;
-        }
-        
-        sockaddr_in addr;
-        addr.sin_family = AF_INET;
-        addr.sin_addr.s_addr = INADDR_ANY;
-        addr.sin_port = htons(port);
-
-        if(bind(handle, (const sockaddr*)&addr, sizeof(sockaddr_in)) < 0)
-        {
-            printf("failed to bind socket\n");
-            close();
-            return false;
-        }
-
-        const int nonBlocking = 1;
-
-        if(fcntl(handle, F_SETFL, O_NONBLOCK, nonBlocking) == -1)
-        {
-            printf("failed to set non-blocking\n");
-            close();
-            return false;
-        }
-
-        return true;
-    }
-
-    void close() {::close(handle);}
-
-    bool send(const Address& destination, const void* data, int size)
-    {
-        sockaddr_in addr;
-        addr.sin_family = AF_INET;
-        addr.sin_addr.s_addr = htonl(destination.ip);
-        addr.sin_port = htons(destination.port);
-
-        const int sentBytes = sendto(handle, (const char*)data, size, 0, (sockaddr*)&addr,
-                                     sizeof(sockaddr_in));
-
-        if(sentBytes != size)
-        {
-            printf("failed to send packet\n");
-            return false;
-        }
-
-        return true;
-    }
-
-    int receive(Address& sender, void* data, int size)
-    {
-        sockaddr_in addr;
-        socklen_t length = sizeof(addr);
-        const int bytes = recvfrom(handle, (char*)data, size, 0, (sockaddr*)&addr, &length);
-
-        if(bytes <= 0)
-            return 0;
-
-        else if(bytes > size)
-        {
-            printf("socket - ignoring packet with size greater than max buffer size");
-            return 0;
-        }
-        
-        sender.ip = ntohl(addr.sin_addr.s_addr);
-        sender.port = ntohs(addr.sin_port);
-        return bytes;
-    }
+    bool open(unsigned short port = 0);
+    void close();
+    bool send(const Address& destination, const void* data, int size);
+    int receive(Address& sender, void* data, int size);
 
     int handle;
 };
@@ -103,18 +40,10 @@ int main()
 
     Socket socket;
     if(!socket.open(port))
-    {
         return 0;
-    }
 
-    // send packet
-    {
-        Address addr;
-        addr.setIp(127, 0, 0, 1);
-        addr.port = port;
-        char data[] = {"test!testkwrj;ekrjegjl;gjew;lkfjewfrk;le"};
-        socket.send(addr, data, sizeof(data)); 
-    }
+    char sendData[] = {"Hello World UDP!!!"};
+    socket.send(Address(127, 0, 0, 1, port), sendData, sizeof(sendData)); 
 
     while(true)
     {
@@ -125,11 +54,90 @@ int main()
         if(!bytesRead)
             continue;
 
-        printf("\nserver received data (port %d)!: ", sender.port);
+        printf("received data (port %d)!: ", sender.port);
         fwrite(buffer, bytesRead, 1, stdout);
+        printf("\n");
         break;
     }
 
     socket.close();
     return 0;
+}
+
+bool Socket::open(unsigned short port)
+{
+    handle = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    
+    if(handle <= 0)
+    {
+        printf("failed to create socket\n");
+        return false;
+    }
+    
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
+
+    if(bind(handle, (const sockaddr*)&addr, sizeof(sockaddr_in)) < 0)
+    {
+        printf("failed to bind socket\n");
+        close();
+        return false;
+    }
+
+    const int nonBlocking = 1;
+
+    if(fcntl(handle, F_SETFL, O_NONBLOCK, nonBlocking) == -1)
+    {
+        printf("failed to set non-blocking\n");
+        close();
+        return false;
+    }
+
+    return true;
+}
+
+void Socket::close()
+{
+    ::close(handle);
+}
+
+bool Socket::send(const Address& destination, const void* data, int size)
+{
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(destination.ip);
+    addr.sin_port = htons(destination.port);
+
+    const int sentBytes = sendto(handle, (const char*)data, size, 0, (sockaddr*)&addr,
+                                 sizeof(sockaddr_in));
+
+    if(sentBytes != size)
+    {
+        printf("failed to send packet\n");
+        return false;
+    }
+
+    return true;
+}
+
+int Socket::receive(Address& sender, void* data, int size)
+{
+    sockaddr_in addr;
+    socklen_t length = sizeof(addr);
+    const int bytes = recvfrom(handle, (char*)data, size, 0, (sockaddr*)&addr, &length);
+
+    if(bytes <= 0)
+        return 0;
+
+    else if(bytes > size)
+    {
+        printf("socket - ignoring packet with size greater than max buffer size");
+        return 0;
+    }
+    
+    sender.ip = ntohl(addr.sin_addr.s_addr);
+    sender.port = ntohs(addr.sin_port);
+    return bytes;
 }
